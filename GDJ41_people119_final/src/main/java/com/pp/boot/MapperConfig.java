8f5839a80ac9a79599ced8315a7b317dd27f6daf
oyeon <oyeon@DESKTOP-4CK7KD1>
2022-01-24 15:45:40 +0900

    설정commit

---
+++ GDJ41_people119_final/src/main/java/com/pp/boot/MapperConfig.java
@@ -0,0 +1,9 @@
+package com.pp.boot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+}
