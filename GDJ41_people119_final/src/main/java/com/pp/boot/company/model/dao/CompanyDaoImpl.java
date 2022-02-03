00c0782ea173b31eefca2c4d6769366428f55271
sunewworld <sunew1003@naver.com>
2022-02-03 22:40:55 +0900

    허허

--- GDJ41_people119_final/src/main/java/com/pp/boot/company/model/dao/CompanyDaoImpl.java
+++ GDJ41_people119_final/src/main/java/com/pp/boot/company/model/dao/CompanyDaoImpl.java
@@ -3,9 +3,10 @@ package com.pp.boot.company.model.dao;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.pp.boot.company.model.vo.Company;
-
+@Repository
 public class CompanyDaoImpl implements CompanyDao{
 	
 	@Override
--- GDJ41_people119_final/src/main/java/com/pp/boot/company/model/service/CompanyServiceImpl.java
+++ GDJ41_people119_final/src/main/java/com/pp/boot/company/model/service/CompanyServiceImpl.java
@@ -4,10 +4,11 @@ import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.pp.boot.company.model.dao.CompanyDao;
 import com.pp.boot.company.model.vo.Company;
-
+@Service
 public class CompanyServiceImpl implements CompanyService {
 	
 	@Autowired
